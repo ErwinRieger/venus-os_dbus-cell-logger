894bce34863776704dcdd8cfbe2ed9f776c3d37b
ErwinRieger <erwin.rieger@ibrieger.de>
2022-10-17 21:48:40 +0200

    Skip mangled data lines in input.

--- fixdata.py
+++ fixdata.py
@@ -9,12 +9,20 @@ lastts = None
 for line in sys.stdin.readlines():
 
     tok = line.split()
-    ts = int(tok[0])
+
+    try:
+        ts = int(tok[0])
+    except ValueError:
+        sys.stderr.write("Warning, can't parse timestamp, line is: '%s'\n" % line.strip())
+        sys.stdout.write("NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN\n")
+        continue
 
     if lastts and ts-lastts > 120:
         # print("gap:", lastts, ts, ts-lastts)
         sys.stdout.write("NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN\n")
 
     lastts = ts
+
+
     sys.stdout.write(line)
 
