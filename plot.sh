b6ddced85b85ad95a533a5d0052138b6bb5b0a84
ErwinRieger <erwin.rieger@ibrieger.de>
2023-03-28 17:38:26 +0200

    Copy and display the last 7200 data points (last 5 days) only.

--- plot.sh
+++ plot.sh
@@ -5,7 +5,8 @@ if [ -n "$1" ]; then
     host="$1"
 fi
 
-ssh root@${host} "cat /data/db/cell-logger.dat" | python3 fixdata.py > cell-logger.dat
+# 7200 minutes are 5 days
+ssh root@${host} 'cat $(ls -rt /data/db/cell-logger.dat*)|tail -7200' | python3 fixdata.py > cell-logger.dat
 
 # Get local timezone offset in seconds
 tzoffset="$(date +%z|cut -c1,3)"    # +0200 -> +2
